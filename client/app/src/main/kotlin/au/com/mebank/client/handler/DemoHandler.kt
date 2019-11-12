40aec8500f0369c79d7f8f15248ec5e58d92b2de
John Layton <john.layton@mebank.com.au>
2019-11-12 16:05:04 +1100

    more work on demo

--- client/app/src/main/kotlin/au/com/mebank/client/handler/DemoHandler.kt
+++ client/app/src/main/kotlin/au/com/mebank/client/handler/DemoHandler.kt
@@ -2,8 +2,8 @@ package au.com.mebank.client.handler
 
 import au.com.mebank.client.model.InputRequest
 import au.com.mebank.client.model.InputResponse
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.BodyInserters.fromValue
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.server.ServerRequest
@@ -16,26 +16,20 @@ class DemoHandler(private val client: WebClient) {
 
   fun sayHello(request: ServerRequest): Mono<ServerResponse> {
 
-//    return client.post()
-//        .uri("/demo")
-//        .body(request.bodyToMono(InputRequest::class.java))
-///*
-//        .header("Authorization", "Basic " + Base64Utils
-//            .encodeToString((username + ":" + token).getBytes(UTF_8)))
-//*/
-//        .retrieve()
-//        .bodyToMono(InputResponse::class.java)
+    return request.bodyToMono(InputRequest::class.java).flatMap { request ->
 
-    return request.bodyToMono(InputRequest::class.java).flatMap { it ->
-      ServerResponse.ok().body(fromValue(client
-          .post()
-          .uri("/demo")
-          .body(BodyInserters.fromPublisher(Mono.just(it), InputRequest::class.java))
-          .bodyToMono(InputResponse::class.java)))
+      client.post()
+          .contentType(MediaType.APPLICATION_JSON)
+          .bodyValue(request)
+          .retrieve()
+          .bodyToMono(InputResponse::class.java)
+          .map { response ->
+
+          }
+
+      ServerResponse.ok().body(fromValue(InputResponse(it.id, it.name)))
     }
-//    return request.bodyToMono(DemoRequest::class.java).flatMap { it ->
-//      ServerResponse.ok().body(fromValue(DemoResponse(it.id, it.name)))
-//    }
   }
+}
 
 }
