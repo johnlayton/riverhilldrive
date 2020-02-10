6446b6e1ce528e55225dc7aec7e522f98ed59c63
John Layton <john.layton@mebank.com.au>
2020-02-10 16:09:21 +1100

    add github plugin

--- buildSrc/src/main/kotlin/au/com/mebank/integration/GithubPlugin.kt
+++ buildSrc/src/main/kotlin/au/com/mebank/integration/GithubPlugin.kt
@@ -2,9 +2,17 @@ package au.com.mebank.integration
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.kohsuke.github.GitHubBuilder
 
+/**
+ * https://github-api.kohsuke.org
+ */
 class GithubPlugin : Plugin<Project> {
   override fun apply(project: Project): Unit = project.run {
+/*
+    val github = GitHubBuilder().withAppInstallationToken("my_installation_token").build()
+    val github = GitHubBuilder.fromEnvironment().build();
+*/
 
   }
 }
